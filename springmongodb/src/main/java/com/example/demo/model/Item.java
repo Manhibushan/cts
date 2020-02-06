2960712409293aa9940b4e36624dc535d8b66e84
Manhibushan <59477547+Manhibushan@users.noreply.github.com>
2020-02-06 17:52:00 +0530

    mongodb enum crud

--- springmongodb/src/main/java/com/example/demo/model/Item.java
+++ springmongodb/src/main/java/com/example/demo/model/Item.java
@@ -1,25 +1,34 @@
 package com.example.demo.model;
 
+import java.time.LocalDate;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
-@Document
+import lombok.ToString;
+
 @Data
+@ToString(exclude = "date")
 public class Item {
 	@Id
 	private String itemId;
 	private String itemName;
 	private String itemPrice;
 	@JsonFormat(shape = JsonFormat.Shape.STRING,pattern="dd-MM-yyyy")
-	private String Localdate;
-	public Item(String itemName, String itemPrice, String localdate) {
+	@JsonIgnore
+	private  LocalDate date;
+	private Product product;
+	public Item(String itemName, String itemPrice, LocalDate date, Product product) {
 		super();
 		this.itemName = itemName;
 		this.itemPrice = itemPrice;
-		Localdate = localdate;
+		this.date = date;
+		this.product = product;
 	}
+	
 	
 }
