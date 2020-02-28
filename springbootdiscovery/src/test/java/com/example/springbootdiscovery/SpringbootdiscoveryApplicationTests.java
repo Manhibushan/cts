ab42ff5ef9a6e97ec0859cb352add62ba6b26bce
Manhibushan <59477547+Manhibushan@users.noreply.github.com>
2020-02-28 13:41:10 +0530

    Eurekaserver

---
+++ springbootdiscovery/src/test/java/com/example/springbootdiscovery/SpringbootdiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
