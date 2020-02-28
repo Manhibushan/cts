be87e7cf1f943e24a0d62dcbd63251ea3c622b96
Manhibushan <59477547+Manhibushan@users.noreply.github.com>
2020-02-28 17:36:52 +0530

    zuluserver

---
+++ Zuluserver/src/test/java/com/example/Zuluserver/ZuluserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zuluserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuluserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
