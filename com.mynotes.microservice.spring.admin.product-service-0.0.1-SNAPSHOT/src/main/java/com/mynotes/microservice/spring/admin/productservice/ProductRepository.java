9ef6c11b3ddbf44ca571d10504d5bd3b5dc06ed4
Manhibushan <59477547+Manhibushan@users.noreply.github.com>
2020-03-02 13:05:24 +0530

    Delete ProductRepository.java

--- com.mynotes.microservice.spring.admin.product-service-0.0.1-SNAPSHOT/src/main/java/com/mynotes/microservice/spring/admin/productservice/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mynotes.microservice.spring.admin.productservice;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-
-}
